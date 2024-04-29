5dc40e7ade593e46c0b33513c2a3acf3b3e82e34
seineph <79405608+seineph@users.noreply.github.com>
2024-04-29 20:48:36 +0300

    added haiku generator ai and axios

--- sources/index.js
+++ sources/index.js
@@ -1,13 +1,26 @@
-function generateHaiku(event) {
-  event.preventDefault();
-
+function publishHaiku(response) {
   let poemElement = document.querySelector("#poem");
   new Typewriter(poemElement, {
-    strings: "haiku goes here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+
+  console.log(response.data.answer);
+}
+
+function generateHaiku(event) {
+  event.preventDefault();
+
+  let userInstructions = document.querySelector("#user-instructions");
+
+  let apiKey = "8daacfbefa5ad84o0e990686f432t73c";
+  let context = `Create each answer in Haiku format, follow the rules at all times. Give the haiku in three lines with <br/>. Sign each poem with <strong>SheCodes.ai</strong>.`;
+  let prompt = `write a haiku about ${userInstructions.value}.`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(publishHaiku);
 }
 
 let haikuFormElement = document.querySelector("#haiku-form");
